7bff779376ace40f955945ea4fd20fc823234e9b
Amirah Nasihah <89834315+amirahnasihah@users.noreply.github.com>
2022-04-01 09:08:37 +0800

    Update jsBreakAndContinue.js

--- jsBreakAndContinue.js
+++ jsBreakAndContinue.js
@@ -1,1 +1,32 @@
 // JavaScript Break and Continue
+
+/*
+the `break` and `continue` statements are often used in loops
+*/
+
+// The break Statement
+/*
+- terminates the execution of the current loop.
+- after terminate, the execution proceeds to the next statement following the loop.
+*/
+
+<html>
+<head>
+   <title> Try It Yourself </title>
+</head>
+<body>
+   <ul id="demo"></ul>
+
+   <script>
+      var html = "";
+      for(var i = 0; i < 10; i++) {
+         if(i == 5) {
+            break;
+         }
+         html += "<li>Number " + i + "<li>"; // add string to html
+      }
+
+      document.getElmentById("demo").innerHTML = html;
+   </script>
+</body>
+</html>
