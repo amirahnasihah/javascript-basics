91f3455c588888ec6fefc59515ac1954c36c4769
Amirah Nasihah <89834315+amirahnasihah@users.noreply.github.com>
2022-04-06 11:48:24 +0800

    update conditional-operators.js

--- conditional-operators.js
+++ conditional-operators.js
@@ -1,6 +1,12 @@
-/* ogical operators often use with Conditional operators
+/*
+Logical operators often use with Conditional operators
 
 How To Use Conditional Operators
 */
 
-//
+// simple condition
+let a = 10;
+let b = 20;
+
+let c = (a == b);
+let d = (b > a)
