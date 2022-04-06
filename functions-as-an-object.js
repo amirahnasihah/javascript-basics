a6c623352d9c3918044b6cee06ed902c19d51fa1
Amirah Nasihah <89834315+amirahnasihah@users.noreply.github.com>
2022-04-07 02:42:08 +0800

    update functions-as-an-object.js

--- functions-as-an-object.js
+++ functions-as-an-object.js
@@ -1,8 +1,11 @@
-// 6.1 overview of functions-as-a-data-type
+/* 6.1 overview of functions-as-a-data-type */
 
 
 
-// 6.2 Storing Function In a Variable
+/* 6.2 Storing Function In a Variable */
+
+
+// using the function variable
 
 let add = function (a,b){
   return a + b;
