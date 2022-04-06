98e3414b254d31f52d5317f4c8a00dcfa3bedaff
Amirah Nasihah <89834315+amirahnasihah@users.noreply.github.com>
2022-04-07 02:54:39 +0800

    update functions.js

--- functions.js
+++ functions.js
@@ -1,6 +1,10 @@
 /* The return Statement */
 
-/*
-- used in function to stop its execution and to return a value to the function caller.
+// used in function to stop its execution and to return a value to the function caller.
 
-*/
+
+// 
+function addNumbers(num1, num2){
+  let sum = num1 + num2;
+  return sum
+}
