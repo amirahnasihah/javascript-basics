323339ff3caab58ccfbd6ab6f74a7f3ec7cbb4f4
Amirah Nasihah <89834315+amirahnasihah@users.noreply.github.com>
2022-04-07 02:57:48 +0800

    update functions.js

--- functions.js
+++ functions.js
@@ -2,13 +2,14 @@
 
 // used in function to stop its execution and to return a value to the function caller.
 
-
+// addNumbers() function return the sum of its arguments (num2)
 function addNumbers(num1, num2){
   let sum = num1 + num2;
   return sum;
 }
 
 // function caller
-console.log(addNumbers(2,5))
+console.log(addNumbers(2,5)) // 7
 
-addNumbers(2, 5) // not work, need to use with print statement (console.log || docu)
+addNumbers(2, 5) // not work, need to use with print statement (console.log || document.write)
+
