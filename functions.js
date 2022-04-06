1ac2798cbc04d58d6b9582e212d562e09eabf0a0
Amirah Nasihah <89834315+amirahnasihah@users.noreply.github.com>
2022-04-07 02:55:44 +0800

    update functions.js

--- functions.js
+++ functions.js
@@ -3,8 +3,10 @@
 // used in function to stop its execution and to return a value to the function caller.
 
 
-// 
 function addNumbers(num1, num2){
   let sum = num1 + num2;
-  return sum
+  return sum;
 }
+
+// function caller
+add
