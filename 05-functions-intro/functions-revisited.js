a777d969802334db4b709ed4b67611271214e3b6
Amirah Nasihah <89834315+amirahnasihah@users.noreply.github.com>
2022-04-11 02:55:52 +0800

    update functions-revisited.js

--- 05-functions-intro/functions-revisited.js
+++ 05-functions-intro/functions-revisited.js
@@ -39,4 +39,5 @@ const sayHello = name => console.log("Hello " + name)
 
 const sayGoodMorning = name => console.log("Good Morning " + name);
 
-greeting("Lucio", sayHello) // call function then pass 2 arguments name "Lucio"
+greeting("Lucio", sayHello); // call function then pass 2 arguments name "Lucio", sayHello
+greeting("Lucio", sayGoodMorning);
