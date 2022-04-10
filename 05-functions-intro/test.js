4cbe154111e980bb38c177d1d8e80b89788e3e5e
Amirah Nasihah <89834315+amirahnasihah@users.noreply.github.com>
2022-04-11 02:28:27 +0800

    update test.js

--- 05-functions-intro/test.js
+++ 05-functions-intro/test.js
@@ -6,7 +6,7 @@ const functionA = function factorial(input){
   return result;
 }
 
-console.log(functionA(3))
+console.log(functionA(3)) // 6
 
 // test to check 1(i) less than 3(input), so 1 + 1 (increment)
-// 
+// 1(result) * 1
