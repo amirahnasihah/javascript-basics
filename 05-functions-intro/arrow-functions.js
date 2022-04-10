5ac10e373bf6681f24c23de0285d161d4f2cb176
Amirah Nasihah <89834315+amirahnasihah@users.noreply.github.com>
2022-04-11 02:02:27 +0800

    update arrow-functions.js

--- 05-functions-intro/arrow-functions.js
+++ 05-functions-intro/arrow-functions.js
@@ -15,3 +15,29 @@ function (a){
 
 // 3. Remove the argument parentheses
 a => a + 100;
+
+
+
+
+///////////////////////////////////
+
+// Traditional Anonymous Function
+function (a, b){
+  return a + b + 100;
+}
+
+// Arrow Function
+(a, b) => a + b + 100;
+
+// Traditional Anonymous Function (no arguments)
+let a = 4;
+let b = 2;
+function (){
+  return a + b + 100;
+}
+
+// Arrow Function (no arguments)
+let a = 4;
+let b = 2;
+() => a + b + 100;
+
