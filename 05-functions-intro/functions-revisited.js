17b9dee1482222389d9e64be22ba2067bfa8d54f
Amirah Nasihah <89834315+amirahnasihah@users.noreply.github.com>
2022-04-11 02:32:52 +0800

    update functions-revisited.js

--- 05-functions-intro/functions-revisited.js
+++ 05-functions-intro/functions-revisited.js
@@ -1,4 +1,4 @@
-const functionA = (input) => {
+const functionB = (input) => {
   let result = 1;
   for (i = 1; i <= input; i++) {
     result = result * i;
@@ -6,4 +6,9 @@ const functionA = (input) => {
   return result;
 }
 
-console.log(functionA(3));
+console.log(functionB(3));
+
+
+// q 2.1.3
+
+const functionC = (width, height) =
