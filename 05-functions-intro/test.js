3b6de075ecfa2b045d5161c98113484fb24f0424
Amirah Nasihah <89834315+amirahnasihah@users.noreply.github.com>
2022-04-11 02:24:49 +0800

    update test.js

--- 05-functions-intro/test.js
+++ 05-functions-intro/test.js
@@ -1,9 +1,9 @@
 const functionA = function factorial(input){
   let result = 1;
-  for (i = 0; i <= input; i++) {
+  for (i = 1; i <= input; i++) {
     result = result * i;
   }
   return result;
 }
 
-console.log(functionA(1))
+console.log(functionA(8))
