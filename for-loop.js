15e28295ef9ab8d409386e793f444df498bec3d9
Amirah Nasihah <89834315+amirahnasihah@users.noreply.github.com>
2022-04-11 08:14:21 +0800

    update for-loop.js

---
+++ for-loop.js
@@ -0,0 +1,22 @@
+// for loop is used in Array @ string
+
+for (initian)
+
+
+// for...of loop in Array/String
+
+for (let element of Array){
+  // do something
+}
+
+/* to change the original array when looping */
+for (var i = 0; i < 5; i++) {
+  array[i] = 0; // 1,2,3,4,5
+}
+
+
+// for...in loop in an Object
+
+for (let property in Object){
+  // block of code @ do something
+}
