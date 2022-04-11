69607295c9aafe45bed2d6b77408d620154ac8d0
Amirah Nasihah <89834315+amirahnasihah@users.noreply.github.com>
2022-04-11 08:04:03 +0800

    update for-loop-arrays.js

--- for-loop-arrays.js
+++ for-loop-arrays.js
@@ -17,7 +17,9 @@ for (let element of Array){
 }
 
 /* to change the original array when looping */
-for (var i)
+for (var i = 0; i < 5; i++) {
+  array[i] = 0; // 1,2,3,4,5
+}
 
 
 // for...in loop in an Object
