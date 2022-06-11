d45daa41e48fae64d6eb53fd1b62b50db76f49e2
Amirah Nasihah <89834315+amirahnasihah@users.noreply.github.com>
2022-06-11 13:14:35 +0800

    update strings.js

--- 03-data-types/strings.js
+++ 03-data-types/strings.js
@@ -1,4 +1,6 @@
 // Concantenate String with Plus symbol
-var my
+var myStr = "The first string. " + "The second string."
+console.log(myStr);
 
 // Concantenate String with Plus Operator
+var myStr1 = ""
