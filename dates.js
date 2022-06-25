5eedc7d44889af1a671fe7c03bc21dc69a763d2d
Amirah Nasihah <89834315+amirahnasihah@users.noreply.github.com>
2022-06-26 00:32:20 +0800

    update dates.js

--- dates.js
+++ dates.js
@@ -1,7 +1,7 @@
 // The date Object
 // Date is a built-in JavaScript object
 // get current year, current date, current day, current time, specify new date and new time, manipulate the format of dates like finding difference between two dates
-// This date object understands and stores date based on a U
+// This date object understands and stores date based on a Unix time timestamp
 
 
 var myDate = new Date();
