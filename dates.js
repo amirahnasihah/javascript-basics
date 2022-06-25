73a729cd49df2aa6d8713920e9c9b172a99bd11d
Amirah Nasihah <89834315+amirahnasihah@users.noreply.github.com>
2022-06-26 00:31:04 +0800

    update dates.js

--- dates.js
+++ dates.js
@@ -1,6 +1,6 @@
 // The date Object
 // Date is a built-in JavaScript object
-// 
+// get current year, current date, current day, current time, specify new date and new time, manipulate the format date
 
 
 var myDate = new Date();
