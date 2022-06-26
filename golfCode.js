fdfc7ca80fffb3f57fed7441a09e6b63b9d09b59
Amirah Nasihah <89834315+amirahnasihah@users.noreply.github.com>
2022-06-27 00:05:53 +0800

    update golfCode.js

---
+++ golfCode.js
@@ -0,0 +1,11 @@
+const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey", "Go Home!"];
+
+function golfScore(par, strokes) {
+  // Only change code below this line
+
+
+  return "Change Me";
+  // Only change code above this line
+}
+
+golfScore(5, 4);
